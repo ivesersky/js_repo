dad6da69494befa5d0e3e589c4807bed93491d06
Iv Esersky <ivanaesersky@hotmail.com>
2021-09-26 17:32:07 -0300

    index.html

--- appClase3.js
+++ appClase3.js
@@ -3,3 +3,18 @@
 -Nombre del .zip "claseApellido"
 Consigna: Crear algoritmo que repita un bloque de instrucciones. En cada repeticion debe efectuarse una operacion o comparacion para obtener salida por alerta o por consola.
 */
+
+
+let num1 = Number(prompt("Ingrese un numero"))
+
+
+if(parseInt(num1)){
+    for (let i=0; i <= num1; i= i+2){
+        console.log(i);
+    }
+
+}
+else{
+    alert("El dato que ingresaste no es un numero.")
+}
+
