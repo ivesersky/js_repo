cf02f622d561216e9340097f170f39159cd3ab3d
Iv Esersky <ivanaesersky@hotmail.com>
2021-09-26 17:14:21 -0300

    appClase4.js

---
+++ appClase4.js
@@ -0,0 +1,5 @@
+/*  DESAFIO 3: CREAR UN ALGORITMO UTILIZANDO UN CICLO.
+-Pagina HTML+Codigo fuente en JavaScript. 
+-Nombre del .zip "claseApellido"
+Consigna: Crear algoritmo que repita un bloque de instrucciones. En cada repeticion debe efectuarse una operacion o comparacion para obtener salida por alerta o por consola.
+*/
