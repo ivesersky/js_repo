48ba34977bf622fb9dfeebc0a8fa000eeeb3c74e
Iv Esersky <ivanaesersky@hotmail.com>
2021-09-27 13:39:32 -0300

    Desafio para descubrir el color complementario

--- appClase3.js
+++ appClase3.js
@@ -1,21 +1,5 @@
-/*  DESAFIO 3: CREAR UN ALGORITMO UTILIZANDO UN CICLO.
--Pagina HTML+Codigo fuente en JavaScript. 
--Nombre del .zip "claseApellido"
-Consigna: Crear algoritmo que repita un bloque de instrucciones. En cada repeticion debe efectuarse una operacion o comparacion para obtener salida por alerta o por consola.
+//  DESAFIO 3: CREAR UN ALGORITMO UTILIZANDO UN CICLO.
 
-let num1;
-let num2;
-
-do{
- num1 = Math.floor(Math.random() * (7 - 1) +1);
- num2 = Math.floor(Math.random() * (7 - 1) +1);
-
- console.log(num1,num2);
-}
-while (num1 === num2 )
-  
-
-*/
 
 let opcion = prompt("¿Quieres jugar? (si/no)")
 if(opcion =="si"){
