298628c82bb460851400091b032c759e08df111e
Iv Esersky <ivanaesersky@hotmail.com>
2021-09-27 13:36:36 -0300

    index.html

--- appClase3.js
+++ appClase3.js
@@ -2,19 +2,51 @@
 -Pagina HTML+Codigo fuente en JavaScript. 
 -Nombre del .zip "claseApellido"
 Consigna: Crear algoritmo que repita un bloque de instrucciones. En cada repeticion debe efectuarse una operacion o comparacion para obtener salida por alerta o por consola.
+
+let num1;
+let num2;
+
+do{
+ num1 = Math.floor(Math.random() * (7 - 1) +1);
+ num2 = Math.floor(Math.random() * (7 - 1) +1);
+
+ console.log(num1,num2);
+}
+while (num1 === num2 )
+  
+
 */
 
-
-let num1 = Number(prompt("Ingrese un numero"))
-
-
-if(parseInt(num1)){
-    for (let i=0; i <= num1; i= i+2){
-        console.log(i);
+let opcion = prompt("¿Quieres jugar? (si/no)")
+if(opcion =="si"){
+    let color = prompt("Por favor, ingresa un color");
+    
+    switch (color){
+        case "rojo":
+            alert("Su color complementario es el verde");
+            break;
+        case "azul":
+            alert("Su color complementario es el naranja");
+            break;
+        case "verde":
+            alert("Su color complementario es el rojo :D");
+            break;
+        case "violeta":
+            alert("Su color complementario es el amarillo");
+            break;
+        case "amarillo":
+            alert("Su color complementario es el violeta");
+            break;
+        case "naranja":
+            alert("Su color complementario es el azul");
+            break;    
+        default:
+            alert("El color ingresado no existe o no esta en mi base de datos, lo siento :(");
+            break;
     }
 
 }
 else{
-    alert("El dato que ingresaste no es un numero.")
-}
+    alert("Que lastima :( nos vemos pronto")
 
+}
