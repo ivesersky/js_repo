25c41a15a923ca5f28a7d955ed4d1a6a3c6ac06f
Iv Esersky <ivanaesersky@hotmail.com>
2021-09-29 23:18:01 -0300

    Desafio complementario con 3 function()

--- appClase4.js
+++ appClase4.js
@@ -1,10 +1,5 @@
 
-//Pedir datos
-//Calcular
-//Mostrar resultado
-
-
-
+/*
 let video = "VIDEO $15.650";
 let digital = "DIGITAL $10.890";
 let analogico = "ANALOGICO $16.985";
@@ -19,3 +14,32 @@ function calcular(a,b){
     
 }
 console.log("PRECIO FINAL CON IVA $"+ num3);
+*/
+
+//Ingresar precio de costo
+let video = "VIDEO $15.650";
+let digital = "DIGITAL $10.890";
+let analogico = "ANALOGICO $16.985";
+
+ingresarDatos();
+function ingresarDatos(){
+    let precio = Number(prompt("Ingresar el precio del servicio: (sin puntos ni comas) " +video+" "+digital+" "+analogico));
+    let porcentaje = 6;
+
+    calcular(precio,porcentaje); //esto va aca porque son variables locales. es parte del proceso de llamar la funcion resultado
+}
+
+//Sumar IVA
+function calcular( a, b){//c es operador pq esta en ese orden 
+
+    let result= a + ((a*b)/100);
+    resultado(result);
+}
+
+
+//Mostrar precio calculado
+
+function resultado(a){
+    console.log("PRECIO FINAL CON IVA "+ a)
+    alert("PRECIO FINAL CON IVA "+ a);
+}
