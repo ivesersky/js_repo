31e8dbf14bc2aa6bae3d5657cb1e158bd2e4bbd1
Iv Esersky <ivanaesersky@hotmail.com>
2021-09-29 22:22:30 -0300

    desafio4

--- appClase4.js
+++ appClase4.js
@@ -1,32 +1,3 @@
-/*
-function validar(nombre){
-    let datoTrabajado = nombre.toLowerCase()
-
-    if (datoTrabajado =="juan"){
-        console.log("podes entrar");
-    }else{
-        console.log("no podes entrar");
-    }
-}
-
-function entrarAlEdificio(){
-    let pregunta =prompt("Como te llamas?")
-    validar(pregunta)
-}
-
-//RETURN
-entrarAlEdificio()
-
-
-
-
-function mostrarNombreCompleto(nombre,apellido,edad){
-    let respuesta =`Tu nombre es ${nombre} ${apellido} y tenes ${edad}`; 
-    return respuesta;
-}
-
-console.log(mostrarNombreCompleto("julian","fuoco", 28))
-*/
 
 //Pedir datos
 //Calcular
@@ -34,12 +5,14 @@ console.log(mostrarNombreCompleto("julian","fuoco", 28))
 
 
 
-let producto = console.log(prompt("Elija 1 servicio a contratar (video/digital/analogico):"));
-let iva = 150;
 
-calcular(producto,iva);
+let precio = Number(prompt("Ingresar el precio del servicio a contratar: (sin puntos ni comas)"));
+let porcentaje = 6;
 
+let num3 = calcular(precio,porcentaje);
 function calcular(a,b){
- let resultado = a*b/100;
- return resultado;
+    let resultado = a + ((a*b)/100);
+    return resultado;
+    
 }
+console.log(num3);
