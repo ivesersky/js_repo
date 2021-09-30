b2175e189d866e1f237866a61a15c6288470a6a5
Iv Esersky <ivanaesersky@hotmail.com>
2021-09-29 23:22:04 -0300

    Comentarios extra borrados

--- appClase4.js
+++ appClase4.js
@@ -1,21 +1,4 @@
 
-/*
-let video = "VIDEO $15.650";
-let digital = "DIGITAL $10.890";
-let analogico = "ANALOGICO $16.985";
-
-let precio = Number(prompt("Ingresar el precio del servicio a contratar: (sin puntos ni comas) " +video+" "+digital+" "+analogico));
-let porcentaje = 6;
-
-let num3 = calcular(precio,porcentaje);
-function calcular(a,b){
-    let resultado = a + ((a*b)/100);
-    return resultado;
-    
-}
-console.log("PRECIO FINAL CON IVA $"+ num3);
-*/
-
 //Ingresar precio de costo
 let video = "VIDEO $15.650";
 let digital = "DIGITAL $10.890";
