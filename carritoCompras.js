4f39117e5f70517fa5fc5978c7e4cb8ab71f4fbf
Iv Esersky <ivanaesersky@hotmail.com>
2021-10-06 17:03:47 -0300

    Adicion de mas productos por array objeto

--- carritoCompras.js
+++ carritoCompras.js
@@ -9,10 +9,14 @@ class Producto{
 }
 
 let listaServicios = [];
-console.log("Listado de servicios disponibles:")
-listaServicios.push(new Producto("video", "Montaje y Posproduccion", 15650))
-listaServicios.push(new Producto("digital", "Foto Producto + Retoque digital", 10890))
-listaServicios.push(new Producto("analogico", "Book 35mm", 16985))
+console.log("Listado de categorias disponibles:")
+listaServicios.push(new Producto("video", "Produccion de pieza audiovisual", 15450))
+listaServicios.push(new Producto("video", "Montaje y Posproduccion", 8500))
+listaServicios.push(new Producto("digital", "Flyers promocionales", 3750))
+listaServicios.push(new Producto("digital", "Foto Producto + Retoque digital", 9890))
+listaServicios.push(new Producto("analogico", "Sesion 35mm (sin revelado)", 13085))
+listaServicios.push(new Producto("analogico", "Sesion 35mm (incluye revelado x1)", 16985))
+
 
 listaServicios.sort((a,b) =>{
     if(a.categoria > b.categoria){
