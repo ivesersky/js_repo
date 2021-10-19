2d2f92161db5a30889507c3ea5ffb9e37012748c
Iv Esersky <ivanaesersky@hotmail.com>
2021-10-18 22:53:21 -0300

    error en appendChild(div)

--- carritoCompras.js
+++ carritoCompras.js
@@ -1,15 +1,54 @@
+const nuevoCarrito = document.querySelector(".nuevo-carrito");
+let listaProductos = [];
+let compra;
+
 
 class Producto{
- constructor(categoria,servicio,precio){
+ constructor(categoria,producto,precio){
      this.categoria = categoria;
-     this.servicio = servicio;
+     this.producto = producto;
      this.precio = precio;
  }
 
 }
 
-let listaServicios = [];
+let producto1 = new Producto("Analogico", "Sesion 35mm (no incluye revelado)", 4085)
+let producto3 = new Producto("Digital", "Flyers promocionales", 3750)
+let producto2 = new Producto("Video", "Produccion de pieza audiovisual", 15450)
+listaProductos.push(producto1,producto2,producto3);
+console.log(listaProductos);
+
+for(let i =0; i< listaProductos.length; i++){
+    const div = document.createElement("div");
+    div.classList.add("item-carrito");
+    div.innerHTML = `
+    <div class="card " >
+        <div class="card-body">
+        <p class="card-text">${listaProductos[i].producto}</p>
+        <h6 class="card-subtitle mb-2 text-muted">${listaProductos[i].categoria}</h6>
+        <h6 class="card-subtitle mb-2 text-muted">Precio: $5985</h6>
+        <button id="ide" value="${listaProductos[i].categoria}">Comprar</button>
+        </div>
+    </div>
+
+    `;
+    nuevoCarrito.appendChild(div);
+}
+
+const guardarDatos = () => {
+
+    const producto = new Producto({
+        categoria : document.getElementById("categoria").value,
+        producto : document.getElementById("producto").value,
+        precio : document.getElementById("precio").value
+    })
+    return producto
+}
+
+
+/*
 console.log("Listado de servicios disponibles:")
+
 listaServicios.push(new Producto("produccion", "Produccion de pieza audiovisual", 15450))
 listaServicios.push(new Producto("montaje", "Montaje y Posproduccion", 8500))
 listaServicios.push(new Producto("flyers", "Flyers promocionales", 3750))
@@ -30,6 +69,10 @@ listaServicios.forEach(serv => {
     console.log(serv.servicio);
 }) 
 
+*/
+
+
+/*
 const guardarDatos = () => {
 
     console.log(document.getElementById("categoria").value)
@@ -43,10 +86,9 @@ document.getElementById("labelForm").style.color = "steelblue"
 document.getElementById("labelForm").style.fontSize = "18px"
 
 
-
-//de esta forma no me funciona :(
-/*const nuevoProducto = new Producto({
+const nuevoProducto = new Producto({
          categoria: document.getElementById("categoria").value,
          servicio: document.getElementById("servicio").value,
          precio: document.getElementById("precio").value
-     }) */ 
+}) 
+*/
