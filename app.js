9a9083c53c227a5bd1c0ab241065c692d7f67f52
Iv Esersky <ivanaesersky@hotmail.com>
2021-11-03 20:35:41 -0300

    API de Dolar para cotizar productos

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// //TOGGLE. MUESTRA-OCULTA DESLIZANDO PARA ARRIBA-ABAJO
+//TOGGLE. MUESTRA-OCULTA DESLIZANDO PARA ARRIBA-ABAJO
 
  $(".btn-ocultar").on("click", ()=>{
      $(".imgProd").slideToggle(1500);
@@ -6,44 +6,83 @@
 
  })
 
-// AJAX CON JQUERY XMLHttpRequest
 
-// CREO UNA CONSTANTE: Declaramos la url que vamos a usar para el GET.
-const URLGET = "https://jsonplaceholder.typicode.com/posts"
+// // AJAX. METODO GET
 
-//Agregamos un botón con jQuery
-$("body").prepend('<button id="btn1">GET</button>');
+// // CREO UNA CONSTANTE: Declaramos la url que vamos a usar para el GET.
+// const URLGET = "https://jsonplaceholder.typicode.com/posts"
 
-//PETICION AJAX --Escuchamos el evento click del botón agregado
-$("#btn1").click(() => { 
+// //Agregamos un botón con jQuery
+// $("body").prepend('<button id="btn1">GET</button>');
 
-    $.get(URLGET, function (respuesta, estado) {
+// //PETICION AJAX --Escuchamos el evento click del botón agregado
+// $("#btn1").click(() => { 
 
-          if(estado === "success"){
-            let misDatos = respuesta;
+//     $.get(URLGET, function (respuesta, estado) {
+//         console.log(estado)
+//           if(estado === "success"){
+//             let misDatos = respuesta;
 
-            for (const dato of misDatos) {
-              $("body").prepend(`<div>
-                                   <h3>${dato.title}</h3>
-                                   <p> ${dato.body}</p>
-                                  </div>`);
-            }  
-          }
-    });
-});
+//             for (const dato of misDatos) {
+//               $("body").prepend(`<div>
+//                                    <h3>${dato.title}</h3>
+//                                    <p> ${dato.body}</p>
+//                                   </div>`);
+//             }  
+//           }
+//     });
+// });
 
-//PETICION A UNA API . Esto se retoma en Backend
-const url = "https://jsonplaceholder.typicode.com/posts" //ruteo el servidor, que puede ser un html un css o como aca un JSON
-console.log(url)
+// //PETICION A UNA API . Esto se retoma en Backend
+// const url = "https://jsonplaceholder.typicode.com/posts" //ruteo el servidor, que puede ser un html un css o como aca un JSON
+// //console.log(url) para ver si me esta reconociendo bien el url
 
-//JQUERY
-$.get(url, (respuesta,estado)=>{
+// //JQUERY+AJAX
+// $.get(url, (respuesta,estado)=>{
  
-  if(estado == "success"){
+//   if(estado == "success"){
     
-      //me muestra por consola la cantidad de data que tengo en esa API que puse como url, osea de ese server
-     respuesta.forEach(element => {
-         console.log(element.id)
-     });
-    }
-}) 
+//       //me muestra por consola la cantidad de data que tengo en esa API que puse como url, osea de ese server
+//      respuesta.forEach(element => {
+//          //console.log(element.id)
+//      });
+//     }
+// }) 
+
+
+
+//AJAX. METODO POST
+//ej. yo quiero mostrar el dolar actualizado. PETICION SIMPLE
+setInterval( ()=>{
+
+    const url = "https://www.dolarsi.com/api/api.php?type=valoresprincipales"
+    
+    $.get(url,(data,est) =>{
+        document.getElementById("dolar").innerHTML ="" //para que se superpongan y no se repitan
+        if(est== "success"){
+            //console.log(data)
+            console.log("DOLAR ACTUALIZADO")
+            data.forEach(element => {
+                //.casa porque es la propiedad del objeto que en este caso es el url
+                document.getElementById("dolar").innerHTML += `
+                    <div class="col-3 carritoCard">
+                        <p>${element.casa.nombre}</p>
+                        <p class="text-danger">${element.casa.compra}</p>
+                        <p class="text-success">${element.casa.venta}</p>
+                    </div>
+                    `
+                    
+                ;
+            });
+    
+    
+        }
+    })
+}, 5000)
+
+//PERO Y SI TENGO QUE HACER UNA PETICION AJAX A 5MILLONES DE USUARIOS?
+//No puedo estar imprimiendo HTML 5millones de veces...
+//FORMA CORRECTA: INYECTAR EL HTML Y CAMBIARLE EL CONTENIDO CADA X TIEMPO, 
+// en vez de cambiar el contenido a cada replica
+//1ro inyectamos etiqueta y 2do cambio el dato
+
