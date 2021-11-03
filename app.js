79a7ac0ca174b71ee31d0eee284dcf95f134c39d
Iv Esersky <ivanaesersky@hotmail.com>
2021-11-03 15:55:53 -0300

    Conexion con API por AJAX

--- app.js
+++ app.js
@@ -1,39 +1,3 @@
-
-//ANIMACIONES CON JQUERY. ESCUCHO EL EVENTO Y TRIGGEREO UNA FUNCION
-//TODO ESTO LO PUEDE HACER JAVSCRIPT INYECTANDO CODIGO.
-
-
-//selecciono boton del html con jquery
-
-// $("#btn1").on("click", () => {
-//     //selecciono la caja con la clase tal y le damos el metodo mostrar pq estaba escondido desde la style.css
-//     $(".box1").show()
-// })
-// //SE LE INYECTA COMO ATRIBUTO EN EL HTML sin si quiera tocar el html
-
-
-// //FADE IN MOSTRAR CON TRANSICION
-// $("#btn3").on("click", () => {
-//     $(".box1").fadeIn(2000)
-// })
-
-// //fade OUT OCULTAR CON TRANSICION
-// $("#btn4").on("click", () => {
-//     $(".box1").fadeOut(3000)
-// })
-
-// //slideDown MUESTRA DESLIZANDO HACIA ABAJO. ESTE ES RE UTIL
-// //PARA EL MENU HAMBURGUESA SI LO HAGO DESDE 0 PQ YO LO HICE CON BOOTSTRAP
-// $("#btn4").on("click", ()=>{
-// $(".box1").slideDown(2000)
-// })
-
-
-// //slideUp MUESTRA DESLIZANDO HACIA ARRIBA
-// $("#btn2").on("click", ()=>{
-//     $(".box1").slideUp(2000)
-// })
-
 // //TOGGLE. MUESTRA-OCULTA DESLIZANDO PARA ARRIBA-ABAJO
 
  $(".btn-ocultar").on("click", ()=>{
@@ -42,8 +6,44 @@
 
  })
 
+// AJAX CON JQUERY XMLHttpRequest
 
-// 
+// CREO UNA CONSTANTE: Declaramos la url que vamos a usar para el GET.
+const URLGET = "https://jsonplaceholder.typicode.com/posts"
 
+//Agregamos un botón con jQuery
+$("body").prepend('<button id="btn1">GET</button>');
 
+//PETICION AJAX --Escuchamos el evento click del botón agregado
+$("#btn1").click(() => { 
 
+    $.get(URLGET, function (respuesta, estado) {
+
+          if(estado === "success"){
+            let misDatos = respuesta;
+
+            for (const dato of misDatos) {
+              $("body").prepend(`<div>
+                                   <h3>${dato.title}</h3>
+                                   <p> ${dato.body}</p>
+                                  </div>`);
+            }  
+          }
+    });
+});
+
+//PETICION A UNA API . Esto se retoma en Backend
+const url = "https://jsonplaceholder.typicode.com/posts" //ruteo el servidor, que puede ser un html un css o como aca un JSON
+console.log(url)
+
+//JQUERY
+$.get(url, (respuesta,estado)=>{
+ 
+  if(estado == "success"){
+    
+      //me muestra por consola la cantidad de data que tengo en esa API que puse como url, osea de ese server
+     respuesta.forEach(element => {
+         console.log(element.id)
+     });
+    }
+}) 
